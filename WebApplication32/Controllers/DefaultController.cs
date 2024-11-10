685ad130c1f3cd9f2c7b0063462f1a0940989c4e
saquib-farooqui <farooquij121@gmail.com>
2024-11-09 18:32:11 -0600

    Changes

--- WebApplication32/Controllers/DefaultController.cs
+++ WebApplication32/Controllers/DefaultController.cs
@@ -78,7 +78,7 @@ namespace WebApplication32.Controllers
              var login = db.registration.FirstOrDefault(u => u.Email == reg.Email && u.Password == reg.Password);
                 if(login != null)
                 {
-                    return RedirectToAction("Registration");
+                    return RedirectToAction("Movies");
                 }
                 ViewBag.Error = "Incorrect Email or Password";
                
@@ -91,6 +91,84 @@ namespace WebApplication32.Controllers
             MoviesViewModel movies = new MoviesViewModel();
             movies.listofmovies = db.movies.ToList();
             return View(movies);
+        }
+        [HttpPost]
+        public ActionResult Movies(MoviesViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                db.movies.Add(vm.newmovie);
+                int rec = db.SaveChanges();
+                if (rec > 0)
+                {
+                    ViewBag.Success = "Movie Added";
+                    return RedirectToAction("Movies");
+                }
+                else
+                {
+                    ViewBag.Error = "Erro Movie Not Added";
+                }
+            }
+
+            MoviesViewModel movies = new MoviesViewModel();
+            movies.listofmovies = db.movies.ToList();
+            return View(movies);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var editmovie = db.movies.Find(id);
+            if(editmovie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(editmovie);
+        }
+        [HttpPost]
+        public ActionResult Edit(Movies mv)
+        {
+            if (ModelState.IsValid)
+            {
+                var editmovie = db.movies.Find(mv.Id);
+
+                if(editmovie == null)
+                {
+                    return HttpNotFound();
+                }
+                editmovie.Name = mv.Name;
+                editmovie.Year = mv.Year;
+                editmovie.Director = mv.Director;
+
+                db.SaveChanges();
+
+                return RedirectToAction("Movies");
+
+            }
+
+            return View();
+        }
+        public ActionResult Delete(int id)
+        {
+            var deletemovie = db.movies.Find(id);
+            if (deletemovie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deletemovie);
+
+        }
+        [HttpPost]
+        public ActionResult Delete(Movies mv)
+        {
+            var delmovie = db.movies.Find(mv.Id);
+            if(delmovie == null)
+            {
+                return HttpNotFound();
+            }
+
+            db.movies.Remove(delmovie);
+            db.SaveChanges();
+            return RedirectToAction("Movies");
         }
     }
 }
